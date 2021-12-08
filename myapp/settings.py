0dcb311c7a336680ffe1eac6de45509e416f5419
Scofield <adebayoabdulrauf1@gmail.com>
2021-12-09 00:05:25 +0100

    updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlesWhiteNoiseMiddlewar'
+    'whitenoise.middle.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'myapp.urls'
